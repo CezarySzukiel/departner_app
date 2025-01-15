78d16e1747fc6aa06d1fd05ca3683104e685d017
CezarySzukiel <cezary.szukiel@gmail.com>
2025-01-15 10:54:22 +0100

    mypy config, and fix errors. django-stubs for mypy installed

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vny97=e-_weprt4x2uqe8x9b1ylbtd0j9&9r5r%im$**qjnvl7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
